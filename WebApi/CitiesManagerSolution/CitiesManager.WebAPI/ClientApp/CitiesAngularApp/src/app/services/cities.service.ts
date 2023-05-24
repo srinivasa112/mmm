199839a68a74fb9de8b25013e3e342eaeef74ca6
P Lakshmi Ayyalasomayajula <n.a.ayyalasomayajula@accenture.com>
2023-05-24 20:51:30 +0530

    aspdotnet core files commit

---
+++ WebApi/CitiesManagerSolution/CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/services/cities.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { City } from "../models/city";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CitiesService {
+  cities: City[] = [];
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public getCities(): Observable<City[]> {
+    return this.httpClient.get<City[]>("https://localhost:7036/api/v1/cities")
+  }
+}
+
---
+++ WebApi/CitiesManagerSolution/CitiesManager.WebAPI/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CitiesManager.WebAPI.Models;
+
+namespace CitiesManager.WebAPI.DaabaseContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+        }
+
+        public ApplicationDbContext() 
+        { 
+        }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<City>().HasData(new City() { CityID =
+            Guid.Parse("88F3EBEF-0A30-452E-9B60-C1C272AF43F2"), CityName = "NewYork" 
+            });
+
+            modelBuilder.Entity<City>().HasData(new City() { CityID =
+            Guid.Parse("8D51C6DF-326D-4736-8B78-E69F6C98C9A5"),CityName = "London"
+            });
+             
+        }
+        }
+    }
+
